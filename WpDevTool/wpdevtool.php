65001db348ba6245ad0cede0895ba6df0f018a23
Alessandro Benoit <micc83@gmail.com>
2013-01-10 21:34:47 +0100

    First steps

--- WpDevTool/wpdevtool.php
+++ WpDevTool/wpdevtool.php
@@ -1,1 +1,14 @@
 <?php
+/*
+Plugin Name: WpDevTool
+Plugin URI: 
+Description: A simple tool to develop on WordPress platform...
+Version: 0.0.1
+Author: Alessandro Benoit
+Author URI: http://codeb.it
+License: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+
+load_plugin_textdomain('wpdevtool', false, basename( dirname( __FILE__ ) ) . '/langs' );
+
