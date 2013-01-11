4bf7c4bf6b6264e461141968e7efee88312a961c
La Reclame <lareclame@MacPro.local>
2013-01-11 20:05:41 +0100

    Different changes
    Added activation hook with version control
    Added styles registration
    Added Maintenance functionality

--- WpDevTool/wpdevtool.php
+++ WpDevTool/wpdevtool.php
@@ -11,12 +11,40 @@ License: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
 /**
  * Define plugin folders and uri
+ *
+ * @since 0.0.1
  */
 define( 'WPDEVTOOL_ABS' , plugin_dir_path( __FILE__ ) );
 define( 'WPDEVTOOL_URI' , plugin_dir_url( __FILE__ ) );
 
 /**
- * Load plugin language file
+ * Plugin activation
+ *
+ * On plugin activation check WordPress version is higher than 3.0
+ * and PHP versione higher than 5
+ *
+ * @since 0.0.1
+ */
+function wpdevtool_activation() {
+	
+	$error = '';
+
+	if ( version_compare( get_bloginfo('version'), '3.0', '<') )
+		$error = __( "This plugin requires WordPress 3.0 or greater.", 'wpdevtool' );
+	
+	if ( version_compare( PHP_VERSION, '5.0.0', '<' ) )
+		$error = __( "This plugin requires PHP 5 or greater.", 'wpdevtool' );
+
+	if ( $error ){
+		deactivate_plugins( basename( __FILE__ ) );
+		wp_die( $error ); 
+	}
+
+}
+register_activation_hook( __FILE__, 'wpdevtool_activation' );
+
+/**
+ * Load WpDevTool language file
  *
  * @since 0.0.1
  */
@@ -26,23 +54,34 @@ function wpdevtool_init() {
 add_action( 'plugins_loaded', 'wpdevtool_init' );
 
 /**
- * Load plugin admin page
+ * WpDevTool Register Stylesheet and Javascript
+ *
+ * @since 0.0.1
+ */
+function wpdevtool_register() {
+	wp_register_style( 'WpDevToolStylesheet', WPDEVTOOL_URI . 'styles/style.css' );
+}
+add_action( 'admin_init', 'wpdevtool_register' );
+
+/**
+ * Load WpDevTool main admin page
  *
  * @since 0.0.1
  */
 require_once( WPDEVTOOL_ABS . 'views/admin.php'  );
 
-function test_function() {
-	echo __( 'Lets do this test', 'wpdevtool' );
-}
-add_action( 'wp_head', 'test_function' );
+/**
+ * Load WpDevTool log error console
+ *
+ * @since 0.0.1
+ */
+require_once( WPDEVTOOL_ABS . 'views/error_log.php'  );
 
 /**
  * WpDevTool Contextual Help
  *
  * @since 0.0.1
  */
-
 function wpdevtool_help( $contextual_help, $screen_id, $screen ) {
 
 	if ( !isset( $_GET['page'] ) )
@@ -75,3 +114,15 @@ if (WP_DEBUG) {
 		
 }
 add_filter('contextual_help', 'wpdevtool_help', 10, 3);
+
+/**
+ * Enable Under Construction
+ *	
+ * @since 0.0.1
+ */
+function wpdevtool_under_construction() {
+	if ( get_option('maintenance') && !current_user_can( 'manage_options' ) )
+		wp_die( '<h1>' . get_bloginfo('name') . '</h1><h2>'.get_bloginfo('name').' è in manutenzione</h2><p>Per qualsiasi necessità potete contattarci all\'indirizzo email '. get_bloginfo('admin_email'), get_bloginfo('name'). ' | Manutenzione programmata', array( 'response' => '503') );
+}
+add_action( 'get_header','wpdevtool_under_construction' );
+
