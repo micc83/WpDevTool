d29c461b25f2dff37a076331e77a5295f60b652a
Alessandro Benoit <micc83@gmail.com>
2013-01-12 22:26:18 +0100

    Moved credits to a different file

---
+++ WpDevTool/inc/credits.php
@@ -0,0 +1,11 @@
+<div id="right_col" class="postbox">
+	<div class="handlediv">
+		<br>
+	</div>
+	<h3 class="hndle"><?php _e( 'Credits', 'wpdevtool' ); ?></h3>
+	<div class="inside">
+		<p><?php _e( 'Proudly presented by', 'wpdevtool' ); ?> :<br>
+		<a href="">Alessandro Benoit</a><br>
+		<a href=""><strong>Comodo Lab Web Agency</strong></a></p>
+	</div>
+</div>
