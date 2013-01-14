03c103fa77b29b79dc9bfa2be632375821261802
Alessandro Benoit <micc83@gmail.com>
2013-01-14 01:06:46 +0100

    Quick links added

--- WpDevTool/inc/credits.php
+++ WpDevTool/inc/credits.php
@@ -2,10 +2,15 @@
 	<div class="handlediv">
 		<br>
 	</div>
-	<h3 class="hndle"><?php _e( 'Credits', 'wpdevtool' ); ?></h3>
+	<h3 class="hndle"><?php _e( 'Credits and Quick Links', 'wpdevtool' ); ?></h3>
 	<div class="inside">
-		<p><?php _e( 'Proudly presented by', 'wpdevtool' ); ?> :<br>
-		<a href="">Alessandro Benoit</a><br>
-		<a href=""><strong>Comodo Lab Web Agency</strong></a></p>
+		<p><?php _e( 'Proudly presented by', 'wpdevtool' ); ?> :</p>
+		<p><a href="" target="_blank">Alessandro Benoit</a><br>
+		<a href="" target="_blank"><strong>Comodo Lab Web Agency</strong></a></p>
+		<p><strong>Quick links:</strong></p>
+		<ul>
+			<li><a href="http://codex.wordpress.org" target="_blank">WordPress Codex</a></li>
+			<li><a href="http://codeb.it" target="_blank">Codebit</a></li>
+		</ul>
 	</div>
 </div>
