da38062cf914d056027ed98332f46e487bcff2d7
La Reclame <micc83@gmail.com>
2013-01-18 13:35:50 +0100

    Fix #3

--- WpDevTool/inc/help.php
+++ WpDevTool/inc/help.php
@@ -21,7 +21,8 @@ function wpdevtool_help( $contextual_help, $screen_id, $screen ) {
 	define( 'WP_DEBUG_LOG', TRUE );
 	define( 'WP_DEBUG_DISPLAY', FALSE );
 	@ini_set( 'display_errors', 0 );
-}</pre>"
+}</pre>
+<p>" . __( "<strong>Warning:</strong> Dont mess up with your wp-config file if you are not sure about what you're doing and remember to clear up the log file periodically so that it does not become too large.",'wpdevtool' ) ."</p>"
 			) );
 			break;
 		default:
