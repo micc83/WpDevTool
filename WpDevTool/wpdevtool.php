1128183da7da99347a77ce6fdbd4df0ea91e2f26
Alessandro Benoit <micc83@gmail.com>
2013-01-19 18:25:09 +0100

    New table class to show data in views

--- WpDevTool/wpdevtool.php
+++ WpDevTool/wpdevtool.php
@@ -85,6 +85,78 @@ add_action( 'admin_enqueue_scripts', 'wpdevtool_enqueue_admin_script' );
 
 /**
  * Load WpDevTool main admin page
+/**
+ * Include WP_List_Table Class to be used inside admin pages
+ * 
+ * @since 0.1.0
+ */
+if( !class_exists( 'WP_List_Table' ) )
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+
+/**
+ * Add some useful methods to WP_List_Table
+ * 
+ * @since 0.1.0
+ */
+class Wpdevtool_Table extends WP_List_Table {
+	
+	/**
+	 * Table pagination
+	 * 
+	 * @since 0.1.0
+	 * @param array $data Array data to count
+	 * @param int $perpage Number of rows per page
+	 */
+	function wpdevtool_paginate( $data, $perpage ) {
+		
+		$totalitems = count( $data ); //return the total number of affected rows
+		$paged = !empty( $_GET["paged"] ) ? mysql_real_escape_string( $_GET["paged"] ) : '';
+		if( empty( $paged ) || !is_numeric( $paged ) || $paged <= 0 ){ $paged=1; }
+		$totalpages = ceil( $totalitems / $perpage );
+		if( !empty( $paged ) && !empty( $perpage ) )
+			$offset = ( $paged - 1 ) * $perpage;
+		
+		$this->set_pagination_args( array(
+			"total_items"	=> 	$totalitems,
+			"total_pages" 	=> 	$totalpages,
+			"per_page" 		=> 	$perpage,
+		) );
+		
+		return $offset;
+		
+	}
+	 
+	 /**
+	  * Prepare the items to be displayed
+	  * 
+	  * @since 0.1.0
+	  */
+	 function prepare_items() {
+	 	global $_column_headers;
+	 
+	 	$data = $this->wpdevtool_get_data(); 
+	 	
+	 	// Search in a given field inside the array
+	 	if ( isset( $_POST['wpdevtool_search'] ) && !empty( $_POST['wpdevtool_search'] ) && isset( $_POST['wpdevtool_search_field'] ) && !empty( $data ) && isset( $data[0][ $_POST['wpdevtool_search_field'] ] ) ){
+	 		$search_result = array();
+	 		foreach ( $data as $value ) {
+	 			if ( false !== strrpos( $value[ $_POST['wpdevtool_search_field'] ], $_POST['wpdevtool_search'] ) )
+	 				$search_result[] = $value;
+	 		}
+	 		$data = $search_result;
+	 	}
+	 		
+	 	$perpage = 20;
+		$offset = $this-> wpdevtool_paginate( $data, $perpage );
+ 		
+ 		$this->_column_headers[0] = $this->get_columns();
+ 		$this->_column_headers[1] = array();
+ 		$this->_column_headers[2] = array();
+		
+ 		$this->items = array_slice( $data, $offset, $perpage );
+	 }
+	 
+}
  *
  * @since 0.0.1
  */
